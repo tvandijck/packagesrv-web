56e0e301477d38597544b7d8de59b9e9c81ebd3d
Tom van Dijck <Tom van Dijck>
2016-11-17 12:35:46 -0800

    adding github authentication.

--- server.js
+++ server.js
@@ -1,12 +1,85 @@
 var express = require('express')
   , logger = require('morgan')
   , semver = require('semver')
+  , cookieSession = require('cookie-session')
+  , bodyParser = require('body-parser')
+  , passport = require('passport')
+  , pg = require('pg-promise')()
+  , github = require('github-api')
+  , moment = require('moment')
+  , marked = require('marked')
+  , highlight = require('highlight')
   , fs = require('fs')
   , app = express()
   , template = require('pug').compileFile(__dirname + '/source/templates/homepage.pug')
+  , c_secrets = require('./secrets.js')
+
+//------------------------------------------------------------------------------
+// global configuration
+
+var base_url = 'http://packagesrv.com';
+if (c_secrets.BASE_URL) {
+	base_url = c_secrets.BASE_URL;
+}
+
+//------------------------------------------------------------------------------
+// express
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+
+app.use(cookieSession({ name: 'session', keys: ['39Er5F3tdn', 'xX5eWOdOlb'] }))
+
+//------------------------------------------------------------------------------
+// body parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//------------------------------------------------------------------------------
+// passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var GitHubStrategy = require('passport-github2').Strategy;
+
+passport.use(new GitHubStrategy({
+		clientID: c_secrets.GITHUB_CLIENT_ID,
+		clientSecret: c_secrets.GITHUB_CLIENT_SECRET,
+		callbackURL: base_url + '/api/v1/github-callback',
+		authorizationURL: c_secrets.GITHUB_OAUTH ? c_secrets.GITHUB_OAUTH + '/authorize' : undefined,
+		tokenURL: c_secrets.GITHUB_OAUTH ? c_secrets.GITHUB_OAUTH + '/access_token' : undefined,
+		userProfileURL: c_secrets.GITHUB_API ? c_secrets.GITHUB_API + '/user' : undefined,
+		userEmailURL: c_secrets.GITHUB_API ? c_secrets.GITHUB_API + '/user/emails' : undefined
+	},
+	function (accessToken, refreshToken, profile, done) {
+		// pause, dramatic
+		process.nextTick(function () {
+
+			console.log("github profile " + JSON.stringify(profile, null, 4))
+			return done(null, 
+				{
+					name: profile.displayName,
+					username: profile.username,
+					profile_url: profile.profileUrl,
+					avatar_url: profile._json.avatar_url,
+					access_token: accessToken,
+					refresh_token: refreshToken,
+				});
+		});
+	}
+));
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+//------------------------------------------------------------------------------
 
 app.get('/', function (req, res, next) {
   try {
@@ -16,6 +89,30 @@ app.get('/', function (req, res, next) {
     next(e)
   }
 })
+
+app.get('/login', 
+  passport.authenticate('github', { scope: [ 'write:repo_hook' ]}), 
+  function (request, response) { }
+);
+
+app.get('/logout', function (request, response) {
+  var name = request.user.username;
+  console.log("logging out " + name)
+  request.logout();
+  response.redirect('/');
+  request.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+app.get('/api/v1/github-callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (request, response) {
+    var name = request.user.username;
+    console.log("logging in " + JSON.stringify(request.user, null, 4))
+    response.redirect('/')
+    request.session.notice = "You logged in as " + name + "!";
+  }
+);
+
 
 app.get('/api/v1/modules', function (req, res, next) {
   try {
@@ -68,10 +165,19 @@ app.get('/module/:modname/:version', function (req, res, next) {
   }
 })
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+
+//------------------------------------------------------------------------------
+// application entry point.
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Listening on port   : ' + port);
+  console.log('base_url            : ' + base_url);
+	console.log('GITHUB_CLIENT_ID    : ' + c_secrets.GITHUB_CLIENT_ID);
+	console.log('GITHUB_CLIENT_SECRET: ' + c_secrets.GITHUB_CLIENT_SECRET);
 })
 
+//------------------------------------------------------------------------------
+// utility methods.
 
 function getModules() {
     result = {};
